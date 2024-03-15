b04aef32c8c383b2e3a4c6d187473f9d5b7ebcce
TheBlueShuckle <villehalldin05@gmail.com>
2024-03-15 20:59:17 +0100

    Implement sever

---
+++ ChatServer/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Client
+    {
+        public string Username { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient ClientSocket { get; set; }
+
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = new Guid();
+
+            Console.WriteLine($"[{DateTime.Now}]: Client has connected with the username: {Username}");
+        }
+    }
+}
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,16 @@ namespace ChatServer
 {
     internal class Program
     {
+        static private List<Client> users;
+        static private TcpListener _listener;
+
         static void Main(string[] args)
         {
+            users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
 
+            Client _client = new Client(_listener.AcceptTcpClient());
         }
     }
 }
